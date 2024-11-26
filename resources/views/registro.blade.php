15a06bd0509b2b346a1b83142c0a696b6f4c0716
felpsrlq <zhigortroll777@gmail.com>
2024-11-26 19:33:59 -0300

    restabelecendo o funcional do projeto

---
+++ resources/views/registro.blade.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="assets/css/estilosRegistro.css">
+
+<div class="wrapper">
+    <div class="logo">
+        <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/7296443-user-icon-person-icon-client-symbol-profile-icon-vetor.jpg" alt="">
+    </div>
+    <div class="text-center mt-4 name">
+        Criar conta
+    </div>
+    <form class="p-3 mt-3">
+        <div class="form-field d-flex align-items-center">
+            <span class="far fa-user"></span>
+            <input type="text" name="userCPF" id="userName" placeholder="CPF">
+        </div>
+        <div class="form-field d-flex align-items-center">
+            <span class="far fa-user"></span>
+            <input type="email" name="userEmail" id="userEmail" placeholder="E-mail">
+        </div>
+        <div class="form-field d-flex align-items-center">
+            <span class="fas fa-key"></span>
+            <input type="password" name="password" id="pwd" placeholder="Senha">
+        </div>
+        <button class="btn mt-3">Criar conta</button>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Rota para exibir o formulário de login
+Route::get('/login', function () {
+    return view('login'); // Certifique-se de que a view é login.blade.php
+})->name('login');
+
+// Rota para processar o login
+Route::post('/login', function (Request $request) {
+    // Validação dos campos
+    $request->validate([
+        'userName' => 'required',
+        'password' => 'required|min:6',
+    ]);
+
+    // Tente autenticar o usuário
+    if (Auth::attempt(['cpf' => $request->userName, 'password' => $request->password])) {
+        // Se autenticado, redireciona para a página inicial ou outra
+        return redirect()->intended('/');
+    } else {
+        // Se falhar, redireciona de volta para o login com um erro
+        return redirect()->route('login')->withErrors(['login_error' => 'Credenciais inválidas.']);
+    }
+});
+
+Route::get('/registro', function () {
+    return view('registro');
 });
